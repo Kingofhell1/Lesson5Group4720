05b0cf937de3ca22f728f4174e0f49beb1d0869a
Kingofhell1 <chernyakov_99@list.ru>
2023-09-11 12:59:18 +0300

    Initial commit

---
+++ src/PhoneGuide/Main.java
@@ -0,0 +1,11 @@
+package PhoneGuide;
+
+
+import calculator.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
---
+++ src/calculator/View.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.List;
+
+public interface View {
+    int get();
+    void set();
+    void set1();
+    void appendLog(String log);
+    public void appendLog(List list);
+    void showLog();
+}
